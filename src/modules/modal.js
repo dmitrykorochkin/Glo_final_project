3c129f53a677d344bc1de55ca4ea9e2ad73f2ac9
Dmitry Korochkin <abramslost@gmail.com>
2022-07-25 23:02:03 +0300

    fix modal

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,51 +2,47 @@ import animate from './helpers';
 
 const modal = () => {
 
-    const callbackBtn = document.querySelectorAll('.callback-btn');
-    const modal = document.getElementById('callback');
-    const modalOverlay = document.querySelector('.modal-overlay'); 
-    const modalClose = document.querySelector('.modal-close');
-
     
     const animateModal = () => {
-        if (screen.width > 768) {
+       
             animate({
-                duration: 500,
+                duration: 700,
                 timing(timeFraction) {
                     return(timeFraction);
                 },
                 draw(progress) {
-                    modal.style.opacity = progress;
+                    modalCallback.style.opacity = progress;
                     modalOverlay.style.display = 'block';
-                    modal.style.display = 'block';
+                    modalCallback.style.display = 'block';
                 } 
             });
-        } else {
-            modalOverlay.style.display = 'block';
-            modal.style.display = 'block';
-        }
-    }
-
-    callbackBtn.forEach(btn => {
-        btn.addEventListener('click', () => {
-            animateModal()
-            
-        });
-    })
-
-
-    modalClose.addEventListener('click', () => {
-        modal.style.display = 'none'
-        modalOverlay.style.display = 'none';
-    });
-
-    modalOverlay.addEventListener('click', (e) => {
-        modal.style.display = 'none'
-        modalOverlay.style.display = 'none';
         
-    })
-  
+    };
+
+
+  const modalCallback = document.getElementById('callback'),
+        modalCallbackBtns = document.querySelectorAll('.callback-btn'),
+        modalOverlay = document.querySelector('.modal-overlay');
+   
+    const closeModal = event => {
+        const target = event.target;
+        if (target.matches('.modal-overlay') || target.closest('.modal-close')) {
+            modalOverlay.style.display = 'none';
+            modalCallback.style.display = 'none';
+            
+        }
+    };
     
-}
+    const openModal = (e) => {
+        e.preventDefault();
+        animateModal()
+        modalOverlay.style.display = 'block';
+        modalCallback.style.display = 'block';
+        document.body.addEventListener('click', closeModal);
+    };
+    modalCallbackBtns.forEach(btn => {
+        btn.addEventListener('click', openModal);
+    });
+};
 
 export default  modal;
