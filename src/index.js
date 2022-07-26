b642cbcc37ba37bec389be1d7f05ad948aab5a77
Dmitry Korochkin <abramslost@gmail.com>
2022-07-26 21:02:32 +0300

    add sendform

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import modal from './modules/modal';
 import scrollLink from './modules/scrollLink';
 import sendForm from './modules/sendForm';
-
+import maskPhone from './modules/maskPhone';
+// import validInput from './modules/validation';
 
 modal();
 scrollLink();
-sendForm()
+maskPhone();
+// validInput()
+sendForm({idForm:'form-callback', 
+          
+});
