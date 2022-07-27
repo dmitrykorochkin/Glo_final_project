89006e3205e04beb1e9b69cda239a666f659ed13
Dmitry Korochkin <abramslost@gmail.com>
2022-07-27 13:18:44 +0300

    add slider

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import modal from './modules/modal';
 import scrollLink from './modules/scrollLink';
 import sendForm from './modules/sendForm';
 import accordeon from './modules/accordeon';
+import slider from './modules/slider';
 
 // import validInput from './modules/validation';
 
 modal();
 scrollLink();
 accordeon();
-// validInput()
+slider()
 sendForm({idForm:'form-callback', 
           
 });
