dd53dbd48c828fa3c96f9427f18804038dec4471
Dmitry Korochkin <abramslost@gmail.com>
2022-07-27 20:45:43 +0300

    add carousel

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import scrollLink from './modules/scrollLink';
 import sendForm from './modules/sendForm';
 import accordeon from './modules/accordeon';
 import slider from './modules/slider';
+import carousel from './modules/carousel';
 
-// import validInput from './modules/validation';
+
 
 modal();
 scrollLink();
 accordeon();
+carousel();
 slider()
 sendForm({idForm:'form-callback', 
           
